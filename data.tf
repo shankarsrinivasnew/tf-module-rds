2c6a0ba8c7d8d1dc28254f51d15d53e06dce09c9
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-29 00:38:44 +0530

    dfg

--- data.tf
+++ data.tf
@@ -1,9 +1,9 @@
 data "aws_ssm_parameter" "rds-user" {
-  name = "${var.env}.rds_user"
+  name = "${var.env}.rds.user"
 }
 
 data "aws_ssm_parameter" "rds-pass" {
-  name = "${var.env}.rds_pass"
+  name = "${var.env}.rds.pass"
 }
 
 data "aws_kms_key" "mykey" {
